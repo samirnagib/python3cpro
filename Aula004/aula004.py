5e6cfa9848d9e61bcb2e9ffec22ee81d8942bb44
Samir Nagib <samir.nagib@gmail.com>
2022-10-01 11:45:23 -0300

    Aula 04

---
+++ Aula004/aula004.py
@@ -0,0 +1,13 @@
+#Imprime uma mensagem
+print("Ola mundo") # Isto é um comentário
+#print("Seja bem vindo")
+
+# Isto é um 
+# Comentario
+# me várias linhas
+"""
+Isto é uma string
+multiline, usada como
+bloco de comentário
+"""
+    
