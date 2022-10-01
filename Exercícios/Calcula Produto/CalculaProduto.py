5b0ac07f52c6f53a6a2bb422abcc4c240bf96914
Samir Nagib <samir.nagib@gmail.com>
2022-10-01 18:37:59 -0300

    Execícios 02

---
+++ Exercícios/Calcula Produto/CalculaProduto.py
@@ -0,0 +1,17 @@
+import os
+
+os.system("cls") or None
+
+# Entrada de dados
+num1 = input("Digite o primeiro número: ")
+num1 = int(num1)
+
+num2 = input("Digite o segundo número.: ")
+num2 = int(num2)
+
+# Processamento
+mult = num1 * num2
+
+# Saída da informação
+print()
+print("A multiplicação de", num1, "por", num2, "é", mult)
---
+++ Exercícios/CalculaTriplo/CalculaTriplo.py
@@ -0,0 +1,6 @@
+num = input("Digite um número: ")
+
+triplo = int(num) * 3
+
+print()
+print("O triplo de", num, "é", triplo)
