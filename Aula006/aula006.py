4328937ec3c82d702239c44365a57992b7642a49
Samir Nagib <samir.nagib@gmail.com>
2022-10-01 14:33:35 -0300

    Aula 06

---
+++ Aula006/aula006.py
@@ -0,0 +1,14 @@
+myvar = "John"
+my_var = "John"
+myVar = "John"
+_my_var = "John"
+
+#Padronização de criação de variaveis
+#Camel Case
+myVariableName = "John"
+
+#Pascal Case
+MyVariableName = "John"
+
+#Snake Case
+my_Variable_Name = "John"
