82cc1cafcced4b36fa9fee101a57d339feb068c3
Samir Nagib <samir.nagib@gmail.com>
2022-10-01 15:33:14 -0300

    Exercício 01

---
+++ Exercícios/Armazenamento/armazenamento.py
@@ -0,0 +1,33 @@
+# declaração das variáveis do programa
+# A, B, C, D, são declaradas na atribuição de valores
+
+#atribui o valor 0 à variável D
+D = 0
+# exibe na tela o valor de D com frase explicativa
+print("Valor de D:", D)
+
+#atribui o valor 2 à variável A
+A = 2
+# exibe na tela o valor de A com frase explicativa
+print("Valor de A:", A)
+
+#atribui o valor 3 à variável B
+B = 3
+# exibe na tela o valor de B com frase explicativa
+print("Valor de B:", B)
+
+#atribui o valor 5 à variável C
+C = 5
+# exibe na tela o valor de C com frase explicativa
+print("Valor de C:", C)
+
+# atualiza a variável C o seu valor anterior acrescido de 1
+C = C + 1
+
+# exibe na tela o valor de C (atualizado) com frase explicativa
+print("Valor de C:", C)
+
+# atribui à variável D a soma do valor de A com o valor atual de C
+D = A + C
+# exibe na tela o valor de D (atualizado) com frase explicativa
+print("Valor de D:", D)
