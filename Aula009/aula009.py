d22b388e398137b91257eb44f8a89072d8ae6bb6
Samir Nagib <samir.nagib@gmail.com>
2022-10-01 15:25:32 -0300

    Aula 09 - Completo

--- Aula009/aula009.py
+++ Aula009/aula009.py
@@ -1,11 +1,17 @@
 x = "incrível"
+
 def myFunc():
-    y = "FANTÁSTICO"
+    global x
+    x = "inacreditavel"
+    y = "fantastico"
     global z
     z = "Bem vindo ao curso"
-    print("Python é " +y)
+    print("Python é " + x + " e " + y)
     print(z)
 
 myFunc()
+
+print("============================")
 print("Você é " + x)
 print(z)
+
