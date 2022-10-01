509e749b4f332b3364000b3d3f390174fdb7b5e3
Samir Nagib <samir.nagib@gmail.com>
2022-10-01 12:55:59 -0300

    Aula 05

---
+++ Aula005/aula005.py
@@ -0,0 +1,21 @@
+x = 5
+y = "Samir"
+z = 4 # é do tipo int
+z  = "Logan" # é o tipo str
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+
+a = str(3)      # a sera "3"
+b = int(3)      # b sera 3
+c = float(3)    # c será 3.0
+
+d = "4"
+D = "Danny"
+
+print(d)
+print(D)
