f21eda0d8ce064acb0446f28c5068776fc16bacd
Samir Nagib <samir.nagib@gmail.com>
2022-10-01 14:46:35 -0300

    Aula 007

---
+++ Aula007/aula007.py
@@ -0,0 +1,27 @@
+# a = "Laranja"
+# b = "Banana"
+# c = "Cereja"
+
+a, b, c = "Laranja","Banana", "Cereja"
+
+print(a)
+print(b)
+print(c)
+
+# d = "Gabriel"
+# e = "Gabriel"
+# f = "Gabriel"
+
+d = e = f = "Gabriel"
+
+print(d)
+print(e)
+print(f)
+
+frutas = ["Maça", "Banana", "Cereja"]
+
+x, y, z = frutas
+
+print(x)
+print(y)
+print(z)
